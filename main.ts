db869b3c0b734a4bdfed7e979397cb672802267c
FaonDev <ufaon@icloud.com>
2024-10-09 08:16:51 -0300

    refactor: :loud_sound: add `pretty` logs

--- main.ts
+++ main.ts
@@ -1,10 +1,19 @@
 import { zValidator } from "@hono/zod-validator";
+import {
+  bold,
+  brightBlue,
+  brightGreen,
+  brightYellow,
+  gray,
+} from "@std/fmt/colors";
 import { Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
 import puppeteer from "puppeteer";
 import { z } from "zod";
 
 const app = new Hono();
+
+let captureCount = 1;
 
 app.get(
   "/",
@@ -17,6 +26,8 @@ app.get(
   async (c) => {
     try {
       const query = c.req.valid("query");
+
+      const start = Date.now();
 
       const browser = await puppeteer.launch({
         args: ["--no-sandbox"],
@@ -34,6 +45,20 @@ app.get(
       const screenshot = await page.screenshot();
 
       await browser.close();
+
+      const end = Date.now();
+
+      const elapsed = Math.floor(end - start);
+
+      console.log(
+        `${bold(brightGreen(`[Capture #${captureCount++}]`))} ${
+          gray(
+            `Scraped ${bold(brightBlue(query.url))} in ${
+              bold(brightYellow(`${Math.round(elapsed / 1000)}s`))
+            }`,
+          )
+        }`,
+      );
 
       return c.newResponse(screenshot, {
         headers: { "Content-Type": "image/png" },
